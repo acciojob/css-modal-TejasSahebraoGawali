6a76c6bb9dce1dcf548cb893cc6f51cf55551e0e
Tejas Sahebrao Gawali <156706283+TejasSahebraoGawali@users.noreply.github.com>
2025-12-01 15:50:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const modal = document.querySelector(".modal");
+const openBtn = document.getElementById("openModal");
+const closeBtn = document.querySelector(".close-modal");
+
+openBtn.addEventListener("click", () => {
+    modal.style.display = "flex";
+});
+
+closeBtn.addEventListener("click", () => {
+	modal.style.display = "none";
+});
+
+window.addEventListener("click", (e) => {
+	if (e.target === modal) {
+		modal.style.display = "none";
+	}
+});
+
+
+
+
+
